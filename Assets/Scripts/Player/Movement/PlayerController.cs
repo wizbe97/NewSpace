df2a23c1a53dee5b4411b7bc49a8ab66ed36bfd1
Nathan Blore <nathanblore97@gmail.com>
2024-02-06 01:33:00 +0000

    Working movement and attacking

---
+++ Assets/Scripts/Player/Movement/PlayerController.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerStates
+    {
+        IDLE,
+        WALK,
+        ATTACK
+    }
+
+    PlayerStates currentState
+    {
+        set
+        {
+            if (stateLock == false)
+            {
+                currentStateValue = value;
+                switch (currentStateValue)
+                {
+                    case PlayerStates.IDLE:
+                        animator.Play("Idle");
+                        canMove = true;
+                        break;
+                    case PlayerStates.WALK:
+                        animator.Play("Walk");
+                        canMove = true;
+                        break;
+                    case PlayerStates.ATTACK:
+                        animator.Play("Attack");
+                        stateLock = true;
+                        canMove = false;
+                        break;
+                }
+            }
+
+        }
+    }
+
+    public float moveSpeed = 3f;
+    public float attackCooldown = 0.5f; // Cooldown time between attacks
+    private float lastAttackTime;
+
+
+    private Vector2 moveInput = Vector2.zero;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private PlayerStates currentStateValue;
+
+    private bool stateLock = false;
+    private bool canMove = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove && !stateLock)
+        {
+            rb.velocity = moveInput * moveSpeed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+
+        if (canMove)
+        {
+            if (currentStateValue != PlayerStates.ATTACK)
+            {
+                if (moveInput != Vector2.zero)
+                {
+                    currentState = PlayerStates.WALK;
+                    animator.SetFloat("xMove", moveInput.x);
+                    animator.SetFloat("yMove", moveInput.y);
+                }
+                else
+                {
+                    currentState = PlayerStates.IDLE;
+                }
+            }
+        }
+    }
+
+    void OnCombat()
+    {
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+            Vector2 attackDirection = (mousePosition - transform.position).normalized;
+
+            animator.SetFloat("xMove", attackDirection.x);
+            animator.SetFloat("yMove", attackDirection.y);
+
+            currentState = PlayerStates.ATTACK;
+
+            lastAttackTime = Time.time;
+        }
+    }
+
+    void OnAttackFinished()
+    {
+        stateLock = false;
+        if (moveInput != Vector2.zero)
+        {
+            currentState = PlayerStates.WALK;
+            animator.SetFloat("xMove", moveInput.x);
+            animator.SetFloat("yMove", moveInput.y);
+        }
+        else
+        {
+            currentState = PlayerStates.IDLE;
+        }
+
+    }
+}
